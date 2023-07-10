4458b76e0fb4747a9cc78d3c6d2322bc839c60e1
reichert621 <reichertjalex@gmail.com>
2023-07-10 16:11:05 -0400

    Minor cleanup

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,10 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  'https://pjvktbprjuxmdziqshwk.supabase.co';
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqdmt0YnByanV4bWR6aXFzaHdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyOTkyNDAsImV4cCI6MjAwMzg3NTI0MH0.hgQtmlCiu1AJsTFo7thsBl2k8QdSRX-5l1sgy4hmjLA';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
